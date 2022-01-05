c5d7661e4ea39beffecc9486518dc0ea31d183d8
ProtonmanEXE <95396758+ProtonmanEXE@users.noreply.github.com>
2022-01-05 14:47:05 +0800

    new

---
+++ src/main/java/protonmanexe/workshopday2_1/Workshopday21Application.java
@@ -0,0 +1,13 @@
+package protonmanexe.workshopday2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopday21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopday21Application.class, args);
+	}
+
+}
---
+++ src/test/java/protonmanexe/workshopday2_1/Workshopday21ApplicationTests.java
@@ -0,0 +1,13 @@
+package protonmanexe.workshopday2_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopday21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
